6340523e012e44f0c009e877e121dd3ce1436f0d
Jonathan Ohayon <johnnyohay@gmail.com>
2022-05-16 17:17:47 +0300

    insert 1 file

--- dbAdapter.js
+++ dbAdapter.js
@@ -1,2 +1,16 @@
-/// text1
-///text 2
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+
+const insertDB = function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("Chocolate");
+    var myobj = { name: "Hot Chocolate", price: "12" };
+    dbo.collection("Product").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+  }
+
+MongoClient.connect(url, insertDB);
